d7152e63da728beae7ebf721d12d7c3464d901a3
kailinc <kailinc@bu.edu>
2017-08-29 09:14:44 -0400

    Initialized files for carts
    - duplicated user carts
    - will modify the files in the next step
    - got files for api, ui, and events

---
+++ assets/scripts/carts/ui.js
@@ -0,0 +1,86 @@
+'use strict'
+const store = require('../store')
+// const showUserShoesTemplate = require('../templates/user-shoes.handlebars')
+// const showUserProfileTemplate = require('../templates/user-profile.handlebars')
+//
+// const onSignUpSuccess = function (data) {
+//   $('#signUpForm').get(0).reset()
+//   $('#signUpMsg').text(' ')
+//   $('#signUpModal').modal('hide')
+//   $('#alertSuccess').css('display', 'block').text('Well Done! You are signed in!')
+//   $('#signUpError').css('display', 'none')
+// }
+//
+// const onSignUpError = function (error) {
+//   console.log(error)
+//   $('#signUpMsg').text('There was a problem with signing up. The email is taken.')
+// }
+//
+// const onSignInSuccess = function (data) {
+//   store.user = data.user
+//   $('#signInMsg').text(' ')
+//   $('#signInForm').get(0).reset()
+//   $('#signInModal').modal('hide')
+//   $('.userIn').css('display', 'block')
+//   $('.userOut').css('display', 'none')
+// }
+//
+// const onSignInError = function (error) {
+//   console.log(error)
+//   $('#signInMsg').text('There was a problem with signing in. The email/password do not match or the account is not signed up.')
+//   $('#signInForm').get(0).reset()
+// }
+//
+// const onChangePwdSuccess = function () {
+//   $('#changePwdForm').get(0).reset()
+//   $('#changePwdModal').modal('hide')
+//   $('#alertSuccess').css('display', 'block').text('Well Done! You have changed your password!')
+//   $('#changePwdMsg').text(' ')
+//   $('#alertDanger').css('display', 'none')
+// }
+//
+// const onChangePwdError = function (error) {
+//   console.log(error)
+//   $('#changePwdForm').get(0).reset()
+//   $('#changePwdMsg').text('There was a problem changing your password.')
+// }
+//
+// const onLogOutSuccess = function () {
+//   store.user = null
+//   $('.userIn').css('display', 'none')
+//   $('.userOut').css('display', 'block')
+// }
+//
+// const onLogOutError = function (error) {
+//   console.log(error)
+// }
+//
+// const onViewProfileSuccess = function (data) {
+//   $('#main').css('display', 'none')
+//   $('#userProfile').css('display', 'block')
+//   $('#userInfo').empty()
+//   const showUserProfileHTML = showUserProfileTemplate({ user: data.user })
+//   $('#userInfo').append(showUserProfileHTML)
+//   console.log(data)
+// }
+//
+// const onViewProfileError = function (error) {
+//   console.log(error)
+// }
+//
+// const onUpdateProfileSuccess = function () {
+//   $('#alertSuccess').css('display', 'block').text('Well Done! You have updated your profile!')
+//   $('#alertDanger').css('display', 'none')
+// }
+//
+// const onUpdateProfileError = function (error) {
+//   console.log(error)
+//   $('#alertDanger').css('display', 'block').text('Sorry, there was a problem with updating your profile.')
+//   $('#alertSuccess').css('display', 'none')
+// }
+
+module.exports = {
+  // onSignUpSuccess,
+  // onSignUpError,
+  // onSignInSuccess
+}
