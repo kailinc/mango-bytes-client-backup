f49987ca51b025e6126ab53f933c56f7e2dea976
kailinc <kailinc@bu.edu>
2017-08-30 09:17:00 -0400

    Initialized Files for storing cart info
    - duplicated store.js
    - changed the names

---
+++ assets/scripts/frontCart.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const frontCart = {
+}
+
+module.exports = frontCart
--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const store = {
+const cart = {
 }
 
-module.exports = store
+module.exports = cart
